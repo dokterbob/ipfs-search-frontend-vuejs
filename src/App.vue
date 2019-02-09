6a8cd2acf75609ae84dd5038a88fd472787f1205
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2019-02-09 19:45:57 +0000

    Add original script code, for reference.

--- src/App.vue
+++ src/App.vue
@@ -60,6 +60,24 @@
 </template>
 
 <script>
+
+// Original script code
+/*
+   // your code goes here
+   $(".dropdown-menu a").click(function(){
+     var selText = $(this).text();
+     $(this).parents('.input-group-append').find('.dropdown-toggle').html(
+       selText+' <span class="caret"></span>');
+   });
+
+   $("button.btn.btn-outline-secondary").on('click', function(e) {
+     $(this).css({
+       'outline':'none !important',
+       'box-shadow':'none'
+     });
+   });
+*/
+
 export default {
   name: 'app',
 };
