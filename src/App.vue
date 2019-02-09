ea28998a971478a80429578fbd25484d44fa0371
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2019-02-09 20:06:47 +0000

    Indentation fixup.

--- src/App.vue
+++ src/App.vue
@@ -3,44 +3,44 @@
 
     <div class="header-wrapper">
       <b-container>
-          <div class="row identity d-flex justify-content-center">
-            <div class="col identity-wrapper">
-              <h1 class="d-flex justify-content-center">
-                <a href="https://ipfs-search.com">
-                  <img src="./assets/logo_white_bright.svg" alt="ipfs-search.com" />
-                </a>
-              </h1>
-            </div>
+        <div class="row identity d-flex justify-content-center">
+          <div class="col identity-wrapper">
+            <h1 class="d-flex justify-content-center">
+              <a href="https://ipfs-search.com">
+                <img src="./assets/logo_white_bright.svg" alt="ipfs-search.com" />
+              </a>
+            </h1>
           </div>
+        </div>
 
-          <div class="row search-bar">
-            <div class="col d-flex justify-content-center">
-              <b-input-group class="col-md-8 col-lg-8">
-                <b-form-input
-                  placeholder="Search..."
-                  aria-label="Text input with dropdown button"
-                />
+        <div class="row search-bar">
+          <div class="col d-flex justify-content-center">
+            <b-input-group class="col-md-8 col-lg-8">
+              <b-form-input
+                placeholder="Search..."
+                aria-label="Text input with dropdown button"
+              />
 
-                <b-input-group-append>
+              <b-input-group-append>
 
-                  <b-dropdown id="ddown1" text="Filter" variant="outline-secondary">
-                    <b-dropdown-item>Any file</b-dropdown-item>
-                    <b-dropdown-item>Image</b-dropdown-item>
-                    <b-dropdown-item>Text</b-dropdown-item>
-                    <b-dropdown-item>Video</b-dropdown-item>
-                    <b-dropdown-item>Audio</b-dropdown-item>
-                    <b-dropdown-item>Directory</b-dropdown-item>
-                  </b-dropdown>
+                <b-dropdown id="ddown1" text="Filter" variant="outline-secondary">
+                  <b-dropdown-item>Any file</b-dropdown-item>
+                  <b-dropdown-item>Image</b-dropdown-item>
+                  <b-dropdown-item>Text</b-dropdown-item>
+                  <b-dropdown-item>Video</b-dropdown-item>
+                  <b-dropdown-item>Audio</b-dropdown-item>
+                  <b-dropdown-item>Directory</b-dropdown-item>
+                </b-dropdown>
 
-                  <b-button variant="outline-secondary" class="search-button">
-                    <span class="glyphicons glyphicons-search"></span>
-                  </b-button>
+                <b-button variant="outline-secondary" class="search-button">
+                  <span class="glyphicons glyphicons-search"></span>
+                </b-button>
 
-                </b-input-group-append>
+              </b-input-group-append>
 
-              </b-input-group>
-            </div>
+            </b-input-group>
           </div>
+        </div>
       </b-container>
     </div>
 
