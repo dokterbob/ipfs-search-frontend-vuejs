3298025ad53c8c515796cd20704ba325de51e4a1
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2019-02-10 20:07:00 +0000

    Footer as component.

---
+++ src/components/footer.vue
@@ -0,0 +1,29 @@
+<template>
+  <footer>
+    <b-container>
+      <b-row class="d-flex justify-content-center">
+        <div class="links">
+          <a class="btn btn-light btn-sm" role="button" href="https://api.ipfs-search.com/">API</a>
+          <a class="btn btn-light btn-sm" role="button" href="https://github.com/ipfs-search/ipfs-search">Code</a>
+          <a class="btn btn-light btn-sm" role="button" href="mailto:info@ipfs-search.com">Contact</a>
+        </div>
+      </b-row>
+
+      <b-row class="d-flex justify-content-center">
+        <div class="rpio">Proudly sponsored by <a href="https://redpencil.io/">redpencil.io</a></div>
+      </b-row>
+    </b-container>
+  </footer>
+</template>
+
+<script>
+export default {
+  data() {
+  },
+};
+</script>
+
+<style scoped>
+</style>
+
+
--- src/pages/landing.vue
+++ src/pages/landing.vue
@@ -2,31 +2,20 @@
   <div class="landing-wrapper">
     <SearchHeader/>
 
-    <div class="footer-wrapper">
-      <b-container>
-        <b-row class="d-flex justify-content-center">
-          <div class="links">
-            <a class="btn btn-light btn-sm" role="button" href="https://api.ipfs-search.com/">API</a>
-            <a class="btn btn-light btn-sm" role="button" href="https://github.com/ipfs-search/ipfs-search">Code</a>
-            <a class="btn btn-light btn-sm" role="button" href="mailto:info@ipfs-search.com">Contact</a>
-          </div>
-        </b-row>
-
-        <b-row class="d-flex justify-content-center">
-          <div class="rpio">Proudly sponsored by <a href="https://redpencil.io/">redpencil.io</a></div>
-        </b-row>
-      </b-container>
-    </div>
-
+    <SearchFooter/>
   </div>
 </template>
 
 <script>
 
 import SearchHeader from '@/components/header';
+import SearchFooter from '@/components/footer';
 
 export default {
-  components: { SearchHeader },
+  components: {
+    SearchHeader,
+    SearchFooter,
+  },
   data() {
   },
 };
