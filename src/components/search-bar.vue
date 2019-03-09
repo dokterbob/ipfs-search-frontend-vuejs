227bd6688b0a64d9b22e871a446c777b032e86e0
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2019-03-09 13:36:59 +0000

    Header form. Not beautiful, but working!

--- src/components/search-bar.vue
+++ src/components/search-bar.vue
@@ -6,34 +6,15 @@
         <b-form-input
           placeholder="Search..."
           aria-label="Text input with dropdown button"
+          v-model="query"
         />
 
         <b-input-group-append>
-          <button class="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Filter</button>
-          <div class="dropdown-menu dropdown-menu-right">
-            <a class="dropdown-item" href="#">Any file</a>
-            <a class="dropdown-item" href="#">Image</a>
-            <a class="dropdown-item" href="#">Text</a>
-            <a class="dropdown-item" href="#">Video</a>
-            <a class="dropdown-item" href="#">Audio</a>
-            <a class="dropdown-item" href="#">Directory</a>
-          </div>
+          <b-form-select v-model="type" :options="types" class="type-select"/>
 
           <b-button variant="outline-secondary" class="search-button" @click="$router.push('/results/')">
             <span class="glyphicons glyphicons-search"></span>
           </b-button>
-
-          <!-- Should be the following code, but that creates issues with rounding -->
-          <!--
-          <b-dropdown id="ddown1" text="Filter" variant="outline-secondary">
-            <b-dropdown-item>Any file</b-dropdown-item>
-            <b-dropdown-item>Image</b-dropdown-item>
-            <b-dropdown-item>Text</b-dropdown-item>
-            <b-dropdown-item>Video</b-dropdown-item>
-            <b-dropdown-item>Audio</b-dropdown-item>
-            <b-dropdown-item>Directory</b-dropdown-item>
-          </b-dropdown>
-          -->
 
         </b-input-group-append>
 
@@ -48,12 +29,26 @@
 <script>
 export default {
   data() {
-    return {};
+    return {
+      query: '',
+      type: null,
+      types: [
+        { value: null, text: 'Filter' },
+        { value: 'any', text: 'Any' },
+        { value: 'video', text: 'Video' },
+        { value: 'audio', text: 'Audio' },
+        { value: 'text', text: 'Text' },
+      ],
+    };
   },
 };
 </script>
 
-<style>
+<style scoped>
+.type-select {
+  border-top-left-radius: 0;
+  border-bottom-left-radius: 0;
+}
 </style>
 
 
