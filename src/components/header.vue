747aabc03ce2f0e5871989cb9deb55ef93ae7401
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2019-03-09 13:52:35 +0000

    Better navigation.

--- src/components/header.vue
+++ src/components/header.vue
@@ -4,9 +4,9 @@
       <b-row class="identity d-flex justify-content-center">
         <b-col class="identity-wrapper">
         <h1 class="d-flex justify-content-center">
-          <a href="https://ipfs-search.com">
-          <img src="@/assets/logo_white_bright.svg" alt="ipfs-search.com" />
-          </a>
+          <router-link to="/">
+            <img src="@/assets/logo_white_bright.svg" alt="ipfs-search.com" />
+          </router-link>
         </h1>
         </b-col>
       </b-row>
